ef64a869bdc2fb4a5484b0c4bdc7b91f5da28405
Andrew <andrii.matukhno@gmail.com>
2020-05-24 15:09:03 +0200

    156 - updated folder structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import Layout from "./components/Layout/Layout";
+import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgetBuilder";
 
 function App() {
